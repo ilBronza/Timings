2ba495d330f13a27aa336f88402ff9a7c34bd187
ilBronza <davide_bronzini@hotmail.com>
2025-07-11 11:16:08 +0200

    all

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # Timings
-Timings management
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Total Downloads][ico-downloads]][link-downloads]
+[![Build Status][ico-travis]][link-travis]
+[![StyleCI][ico-styleci]][link-styleci]
+
+This is where your description should go. Take a look at [contributing.md](contributing.md) to see a to do list.
+
+## Installation
+
+Via Composer
+
+``` bash
+$ composer require ilbronza/timings
+```
+
+## Usage
+
+## Change log
+
+Please see the [changelog](changelog.md) for more information on what has changed recently.
+
+## Testing
+
+``` bash
+$ composer test
+```
+
+## Contributing
+
+Please see [contributing.md](contributing.md) for details and a todolist.
+
+## Security
+
+If you discover any security related issues, please email davide@sistema.srl instead of using the issue tracker.
+
+## Credits
+
+- [Davide Bronzini][link-author]
+- [All Contributors][link-contributors]
+
+## License
+
+MIT. Please see the [license file](license.md) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/ilbronza/timings.svg?style=flat-square
+[ico-downloads]: https://img.shields.io/packagist/dt/ilbronza/timings.svg?style=flat-square
+[ico-travis]: https://img.shields.io/travis/ilbronza/timings/master.svg?style=flat-square
+[ico-styleci]: https://styleci.io/repos/12345678/shield
+
+[link-packagist]: https://packagist.org/packages/ilbronza/timings
+[link-downloads]: https://packagist.org/packages/ilbronza/timings
+[link-travis]: https://travis-ci.org/ilbronza/timings
+[link-styleci]: https://styleci.io/repos/12345678
+[link-author]: https://github.com/ilbronza
+[link-contributors]: ../../contributors
---
+++ src/TimingsServiceProvider.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace IlBronza\Timings;
+
+use Illuminate\Support\ServiceProvider;
+
+class TimingsServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'ilbronza');
+        $this->loadViewsFrom(__DIR__.'/Resources/Views', 'timings');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        // Publishing is only necessary when using the CLI.
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/timings.php', 'timings');
+
+        // Register the service the package provides.
+        $this->app->singleton('timings', function ($app) {
+            return new Timings;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['timings'];
+    }
+
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    protected function bootForConsole(): void
+    {
+        // Publishing the configuration file.
+        $this->publishes([
+            __DIR__.'/../config/timings.php' => config_path('timings.php'),
+        ], 'timings.config');
+
+        // Publishing the views.
+        /*$this->publishes([
+            __DIR__.'/../resources/views' => base_path('resources/views/vendor/ilbronza'),
+        ], 'timings.views');*/
+
+        // Publishing assets.
+        /*$this->publishes([
+            __DIR__.'/../resources/assets' => public_path('vendor/ilbronza'),
+        ], 'timings.views');*/
+
+        // Publishing the translation files.
+        /*$this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/ilbronza'),
+        ], 'timings.views');*/
+
+        // Registering package commands.
+        // $this->commands([]);
+    }
+}
